82a9f2dc9f7eb34e0b7682c55ac99cc5698df7e8
onajebaxley <onaje.baxley@gmail.com>
2018-07-16 11:06:18 -0400

    Add initial non-comprehensive unit tests for LicenseManager.

---
+++ test/unit/license-manager-spec.ts
@@ -0,0 +1,103 @@
+import { Promise as _promise } from 'bluebird';
+import _chai from 'chai';
+import _chaiAsPromised from 'chai-as-promised';
+import 'mocha';
+import _rewire from 'rewire';
+import _sinon from 'sinon';
+import _sinonChai from 'sinon-chai';
+
+_chai.use(_chaiAsPromised);
+_chai.use(_sinonChai);
+const { expect } = _chai;
+
+const _licenseManagerModule = _rewire('../../src/license-manager');
+const LicenseManager = _licenseManagerModule.LicenseManager;
+
+describe('LicenseManager', () => {
+    const _createManager = (hostname?, port?, path?) => {
+        hostname = hostname || 'google.com';
+        port = port || 80;
+        path = path || '';
+
+        return new LicenseManager(hostname, port, path);
+    };
+
+    describe('constructor()', () => {
+        it('should expose the desired methods and properties', () => {
+            const lm = new LicenseManager('host');
+
+            expect(lm).to.be.an('object');
+            expect(lm.init).to.be.a('function');
+            expect(lm.run).to.be.a('function');
+            expect(lm.executeValidator).to.be.a('function');
+            expect(lm.halt).to.be.a('function');
+        });
+    });
+
+    describe('init()', () => {
+        it('should return a Promise when invoked', function(this: any) {
+            this.timeout(4000);
+            const lm = _createManager();
+            const shouldBeAPromise = lm.init();
+
+            shouldBeAPromise.then((res) => ({})).catch((e) => ({}));
+            expect(shouldBeAPromise).to.be.a('Promise');
+            // expect(shouldBeAPromise).to.be.rejected;
+        });
+
+        it.skip('should indicate the success of validation technique when resolving'
+            + ' Promise', async function(this: any, done) {
+            // TODO: Spoof the service to test this properly
+            this.timeout(4000);
+            const lm = _createManager();
+            const aPromise = lm.init();
+            const res = await aPromise;
+
+            expect(aPromise).to.be.fulfilled;
+            expect(res).to.be.a('boolean').and.equal('true');
+        });
+    });
+
+    describe('executeValidator()', () => {
+        it('should return a native Promise when invoked', () => {
+            const lm = _createManager();
+            const shouldBeAPromise = lm.executeValidator();
+
+            shouldBeAPromise.then(() => ({})).catch(() => ({}));
+
+            expect(shouldBeAPromise).to.be.a('Promise');
+        });
+
+        it('should eventually resolve to a boolean indicating license validity');
+    });
+
+    describe('run()', () => {
+        it('should do something');
+    });
+
+    describe('halt()', () => {
+        it('should return a native Promise when invoked', () => {
+            const lm = _createManager();
+            const shouldBeAPromise = lm.halt(100);
+
+            shouldBeAPromise.then(() => ({})).catch(() => ({}));
+            expect(shouldBeAPromise).to.be.a('Promise');
+        });
+
+        it('should require the given time to resolve at minimum', function(this: any, done) {
+            this.timeout(4000);
+            const lm = _createManager();
+            const spy = _sinon.spy();
+            const waitTime = 1000;
+
+            setTimeout(spy, waitTime - 200);
+
+            lm.halt(waitTime).then((res) => {
+                expect(spy).to.have.been.called;
+                done();
+            }).catch((err) => {
+                done('ERR: halt() should have been resolved.');
+            });
+        });
+    });
+});
